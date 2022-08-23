9983f057291ae953b832a3504f0d328362f3a96f
Ignacio Guillen <ignacio.guillen00@gmail.com>
2022-08-23 02:54:30 -0300

    ejercicio 3 incompleto

---
+++ LabollClase1/Ejercicio03/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ejercicio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numIngresado;
+            bool resultado = int.TryParse(Console.ReadLine(), out int numero);
+
+            numIngresado = numero;
+
+            int numerosPrimosContados = 0;
+
+            int acumulador = 2;
+       
+
+            for (int i = 2; i < numIngresado; i++)
+            {
+                bool esPrimo = true;
+                if (acumulador % i == 0)
+                {
+                    for (int b = i + 1; b < numIngresado+1; b++)
+                    {
+                        if (acumulador % b == 0)
+                        {
+                            esPrimo = false;
+                            b = numIngresado;
+                        }
+                    }
+                    if (esPrimo)
+                    {
+                        Console.WriteLine("los numeros primos son: {0}", acumulador);
+                    }
+                }
+                acumulador++;
+            }
+        }
+    }
+}
