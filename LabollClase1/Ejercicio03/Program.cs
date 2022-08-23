afcb073e33b8d2c4b7d74a9408ade195ff9ca601
Ignacio Guillen <ignacio.guillen00@gmail.com>
2022-08-23 03:20:05 -0300

    ejercicio 3 finalizado

--- LabollClase1/Ejercicio03/Program.cs
+++ LabollClase1/Ejercicio03/Program.cs
@@ -7,35 +7,38 @@ namespace Ejercicio03
         static void Main(string[] args)
         {
             int numIngresado;
-            bool resultado = int.TryParse(Console.ReadLine(), out int numero);
-
-            numIngresado = numero;
-
+            
             int numerosPrimosContados = 0;
+            int contadorDeDivisores = 0;
+            string respuesta;
 
-            int acumulador = 2;
-       
-
-            for (int i = 2; i < numIngresado; i++)
+            do
             {
-                bool esPrimo = true;
-                if (acumulador % i == 0)
+                bool resultado = int.TryParse(Console.ReadLine(), out int numero);
+                numIngresado = numero;
+
+                for (int i = 2; i < numIngresado; i++)
                 {
-                    for (int b = i + 1; b < numIngresado+1; b++)
+                    for (int b = 1; b < numIngresado; b++)
                     {
-                        if (acumulador % b == 0)
+                        if (contadorDeDivisores > 2)
                         {
-                            esPrimo = false;
-                            b = numIngresado;
+                            break;
+                        }
+                        else if (i % b == 0)
+                        {
+                            contadorDeDivisores++;
                         }
                     }
-                    if (esPrimo)
+                    if (contadorDeDivisores == 2)
                     {
-                        Console.WriteLine("los numeros primos son: {0}", acumulador);
+                        Console.WriteLine(i);
                     }
+                    contadorDeDivisores = 0;
                 }
-                acumulador++;
-            }
+                Console.WriteLine("¿Desea seguir operando? s/n");
+                respuesta = Console.ReadLine();
+            } while (respuesta != "n");
         }
     }
 }
